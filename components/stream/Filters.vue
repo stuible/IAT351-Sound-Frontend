38a30bebf3d93e6e4a20a364a029cde7c684860b
Josh Stuible <shrewdlove@gmail.com>
2018-11-28 22:56:09 -0800

    Can now click filters to change them
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- components/stream/Filters.vue
+++ components/stream/Filters.vue
@@ -1,20 +1,72 @@
 <template>
     <div class="filters">
-            <span>PLAY</span>
-            the latest
-            <span>Unseen</span>
-            <span>Posts</span>
-            from the people you follow
+            <span @click="togglePlaySkim()">{{playOrSkimText}}</span>
+            <!-- the latest -->
+            <span @click="toggleAllUnseen()">{{allOrUnseenText}}</span>
+            <span @click="togglePostsReposts()">{{postsOrRepostsText}}</span>
+            from the people you follow:
         </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            playOrSkim: true,
+            allOrUnseen: true,
+            postsOrReposts: false,
+
+        }
+    },
+    computed: {
+        playOrSkimText(){
+            if(this.playOrSkim) return 'Play'
+            else return 'Skim'
+        },
+        allOrUnseenText(){
+            if(this.allOrUnseen) return 'All'
+            else return 'Unseen'
+        },
+        postsOrRepostsText(){
+            if(this.postsOrReposts) return 'Posts'
+            else return 'Reposts'
+        }
+    },
+    methods: {
+        togglePlaySkim(){
+            console.log('clicked')
+            this.playOrSkim = !this.playOrSkim
+        },
+        toggleAllUnseen(){
+            console.log('clicked')
+            this.allOrUnseen = !this.allOrUnseen
+        },
+        togglePostsReposts(){
+            console.log('clicked')
+            this.postsOrReposts = !this.postsOrReposts
+        }
+    }
+    
 
 }
 </script>
 
 <style lang="scss" scoped>
+.filters {
+    font-size: 3em;
+    text-transform: lowercase;
+
+    span {
+        border: 2px dashed grey;
+        padding: 0 0.25em;
+        font-weight: 900;
+        cursor: pointer;
+
+        &:hover{
+            border-color: $orangeColour;
+        }
+    }
+}
 
 </style>
 
