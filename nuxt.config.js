50faf2db1bb887c2042e1330d7f030efd40d7603
Josh Stuible <shrewdlove@gmail.com>
2018-11-28 18:24:16 -0800

    Attempting to pregenerate all dynamic song routes
    Signed-off-by: Josh Stuible <shrewdlove@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Sovnd',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -29,7 +29,7 @@ module.exports = {
   */
   css: [
     'normalize.css/normalize.css',
-    'flexboxgrid/css/flexboxgrid.min.css' 
+    'flexboxgrid/css/flexboxgrid.min.css'
   ],
   /*
   ** Environment Variables
@@ -89,6 +89,63 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+    }
+  },
+  generate: {
+    routes: function () {
+      //   const {
+      //     data
+      // } = await app.$axios.get(env.api.apiUrl + 'items/users?fields=*.*.*.*.*.*.*&single=1&filter[name]=' + env.user.name,
+      //     JSON.stringify({
+      //         // filter: { published: true },
+      //         // sort: {_created:-1},
+      //         // populate: 1
+      //     }), {
+      //         headers: {
+      //             'Content-Type': 'application/json'
+      //         }
+      //     })
+
+      // let followerPosts = [];
+
+      // Object.entries(data.data.follows).forEach(([key, val]) => {
+      //   Object.entries(val.is_following.reposts).forEach(([key, repost]) => {
+      //     let song = {
+      //       user: val.is_following,
+      //       song: repost.song,
+      //       timestamp: repost.timestamp
+      //     }
+      //     followerPosts.push(song)
+      //   });
+      // });
+      return axios.get(env.api.apiUrl + 'items/users?fields=*.*.*.*.*.*.*&single=1&filter[name]=' + env.user.name,
+        JSON.stringify({
+          // filter: { published: true },
+          // sort: {_created:-1},
+          // populate: 1
+        }), {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((res) => {
+          let followerPosts = [];
+
+          Object.entries(res.data.follows).forEach(([key, val]) => {
+            Object.entries(val.is_following.reposts).forEach(([key, repost]) => {
+              let song = {
+                user: val.is_following,
+                song: repost.song,
+                timestamp: repost.timestamp
+              }
+              followerPosts.push(song)
+            });
+          });
+
+          return followerPosts.data.map((page) => {
+            let route = '/track/' + page.title
+          })
+        })
     }
   }
 }
